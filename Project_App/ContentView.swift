a24c85ffa80ce1c9ad74584e31c42ea63649320a
Testing_author <testing_beta@mail.com>
2021-02-09 23:13:12 +0200

    Initial Commit

---
+++ Project_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_App
+//
+//  Created by DDDD on 09/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project_App/Project_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_AppApp.swift
+//  Project_App
+//
+//  Created by DDDD on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
